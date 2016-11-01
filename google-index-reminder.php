ce28c9b97801bfd28a0c754fd4ed070e81bec226
TamBui <tam.bui@sunbytes.vn>
2016-11-01 11:47:45 +0700

    s

--- google-index-reminder.php
+++ google-index-reminder.php
@@ -11,10 +11,10 @@ Network: True
 function jb_admin_notice__error() {
 	$blog_public = get_option('blog_public', 0);
 	if( $blog_public == 0 ){
-		$message = '<h2>Discourage Search Engines is <strong style="color:red">ENABLED</strong>!  That mean Google will <strong style="color:red">NO INDEX</strong> your webisite. Be careful if you are on LIVE SITE</h2>';
+		$message = '<h2>Discourage Search Engines is <strong style="color:red">ENABLED</strong>!  That mean Google will <strong style="color:red">NO INDEX</strong> your website. Be careful if you are on LIVE SITE</h2>';
 	}
 	else{
-		$message = '<h2 >Discourage Search Engines is <strong style="color:red">DISABLED</strong>!  That mean Google will <strong style="color:red">INDEX</strong> your webisite. Be careful if you are on DEVELOPMENT OR STAGING SITE </h2>';
+		$message = '<h2 >Discourage Search Engines is <strong style="color:red">DISABLED</strong>!  That mean Google will <strong style="color:red">INDEX</strong> your website. Be careful if you are on DEVELOPMENT OR STAGING SITE </h2>';
 	}
 	$class = 'notice notice-warning is-dismissible';	
 	printf( '<div class="%1$s"><p>%2$s</p></div>', $class, $message ); 
